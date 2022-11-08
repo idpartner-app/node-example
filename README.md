39e59edafa64a242b7d8af9b38d3844f3be8928f
Chris McCaw <developer.cmccaw@gmail.com>
2022-11-08 12:27:47 -0800

    updated README

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
-# Intro
-Example of a nodejs RP client
+# IDPartner Node Demo
+A demo of a Relying Party integrating with IDPartner
 
-# Install
-Run `npm install`
+## Getting started
 
-# Start
-Run `DEBUG=rp-example:* npm start`
+### Create IDPartner Account
 
-# Give it a try
-Go to `http://localhost:8000` and play with it
+Start by setting up an account and creating an application. https://docs.idpartner.com/documentation/getting-started/onboarding-account
+
+### Install ngrok
+
+It is required for the callback url to be https. The easiest way to expose an HTTPS endpoint that you can use locally is via [ngrok](https://ngrok.com)
+
+### Run the demo
+
+These are the steps for running the IDPartner node demo:
+
+Clone the demo git repository:
+
+```sh
+git clone https://github.com/idpartner-app/rp-example.git
+```
+
+Install the project
+
+```sh
+yarn install
+```
+
+
+Start the application
+
+```sh
+yarn start
+```
+
+### Update Client ID
+
+After creating an IDPartner account and application a client id will be generated. There are two places you need to update
+
+1. [Update CHANGE_ME_CLIENT_ID in custom html](./views/index.ejs.md)
+1. [Update CHANGE_ME_CLIENT_ID in confidential client](./routes/index.js)
+1. [Update callback url in confidential client](./routes/index.js)
+
--- routes/index.js
+++ routes/index.js
@@ -2,46 +2,45 @@ const express = require('express');
 const jose = require('node-jose');
 const IDPartner = require('@idpartner/node-oidc-client');
 
-const keyStore = jose.JWK.createKeyStore();
-keyStore.generate('RSA', 2048, { alg: 'RSA-OAEP', enc: 'A256CBC-HS512', use: 'enc' });
-keyStore.generate('RSA', 2048, { alg: 'PS256', use: 'sig' });
+let idPartnerClient = {};
 
-const getIdPartnerClient = jwks => new IDPartner({
-  jwks,
-  client_id: 'qJ2qZRlmXUkBZ4aOQEls-',
-  callback: 'https://9d97476bea2e.ngrok.io/auth/callback',
-  trust_directory_service_url: 'https://auth-api.idpartner-dev.com/trust-directory',
-  oidc_proxy_service_url: 'https://auth-api.idpartner-dev.com/oidc-proxy',
+!async function () {
+  // This will generate a keypair however for production you will want to keep the private key secret
+  const keyStore = jose.JWK.createKeyStore();
+  await keyStore.generate('RSA', 2048, { alg: 'RSA-OAEP', enc: 'A256CBC-HS512', use: 'enc' });
+  await keyStore.generate('RSA', 2048, { alg: 'PS256', use: 'sig' });
+  
+  idPartnerClient = new IDPartner({
+    jwks: keyStore.toJSON(true),
+    client_id: 'CHANGE_ME_CLIENT_ID',
+    callback: 'https://myapplication.ngrok.io/auth/callback',
 });
+}();
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  const jwks = keyStore.toJSON(true);
-  const idPartner = getIdPartnerClient(jwks);
   res.render('index', { title: 'RP Example' });
 });
 
 router.get('/jwks', async (req, res, next) => {
-  const jwks = keyStore.toJSON(false);
-  res.json(jwks);
+  const jwks = await idPartnerClient.getPublicJWKs();
+  res.send(jwks);
 });
 
 router.get('/auth', async (req, res, next) => {
-  const jwks = keyStore.toJSON(true);
-  const idPartner = getIdPartnerClient(jwks);
   const scope = ['openid', 'email', 'profile'];
-  req.session.idp_proofs = idPartner.generateProofs();
-  const authorizationUrl = await idPartner.getAuthorizationUrl(req.query, req.session.idp_proofs, scope);
+  req.session.idp_proofs = idPartnerClient.generateProofs();
+  const authorizationUrl = await idPartnerClient.getAuthorizationUrl(req.query, req.session.idp_proofs, scope);
   return res.redirect(authorizationUrl);
 });
 
 router.get('/auth/callback', async (req, res, next) => {
-  const jwks = keyStore.toJSON(true);
-  const idPartner = getIdPartnerClient(jwks);
-  const { idp_response_code, identity_provider } = await idPartner.unpackProxyResponse(req.query);
-  const claims = await idPartner.claims(idp_response_code, req.session.idp_proofs);
+  const { idp_response_code, identity_provider } = await idPartnerClient.unpackProxyResponse(req.query);
+  
+  const claims = await idPartnerClient.claims(idp_response_code, req.session.idp_proofs);
   return res.send(claims);
+
 });
 
 module.exports = router;
